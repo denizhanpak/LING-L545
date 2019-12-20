3b8a9599562bd927c382d3b8dba29c65530d2ce4
denizhanpak <denpak@iu.edu>
2019-12-20 14:59:58 -0500

    Added complete tokenizer

--- 01_Tokenisation/Tokenizer/tokenizer.py
+++ 01_Tokenisation/Tokenizer/tokenizer.py
@@ -1,26 +1,11 @@
 import sys, re
 
-abbr = ['etc.', 'e.g.', 'i.e.']
-
-def tokenize (line, abbr):
-    line = re.sub(r'([\(\)"?:!;])', r' \g<1> ', line)
-    line = re.sub(r'([^0-9]),', r'\g<1> ', line)
-    line = re.sub(r'([^0-9]),', r' \g<1> ', line)
-    line = re.sub(r'  +', ' ', line[:-1])
-
-    output  = []
-    for token in line.split(' '):
-        if len(token) == 0: continue
-        if token[-1] == '.' and token not in abbr:
-            token = token[:-1] + " ."
-        output.append(token)
-    return " ".join(output)
-
-#line = sys.stdin.readline()
+if len(sys.argv) < 2:
+    print("Usage: python tokenizer.py dictionary document [stdin document to tokenize]")
+    exit()
 
 def maxMatch (text, dictionary):
-    if text == "": return []
-    if len(text) == 1: return [text]
+    if len(text) <= 1: return [text]
 
     for i in range(1, len(text)):
         firstword = text[:i]
@@ -29,9 +14,11 @@ def maxMatch (text, dictionary):
             return [firstword] + maxMatch (remainder, dictionary)
     return []
 
-dictionary = set(["a","b", "c","d"])
-print(maxMatch ("abdbc", dictionary))
+dictfile = open(sys.argv[1], 'r')
+dictionary = [line.strip("\n") for line in dictfile.readlines()]
 
-#while line != '':
-    #print(tokenize(line.strip('\n'), abbr))
-    #line = sys.stdin.readline()
+line = sys.stdin.readline()
+while line != '':
+    print(maxMatch(line.strip('\n'), dictionary))
+    line = sys.stdin.readline()
+
